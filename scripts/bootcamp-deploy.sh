ab4edeecab18d18390b16f9a9a7237ab354e406c
Iso-Doss <dossou.israel48@gmail.com>
2017-12-11 16:11:26 +0100

    Amelioration du script et des elements

--- scripts/bootcamp-deploy.sh
+++ scripts/bootcamp-deploy.sh
@@ -6,10 +6,10 @@ repository_dir=$1
 profile=$2
 
 #Un tableau contenant le nom de tous les projets qui generent un jar
-projets_jars=("bootcamp.common" "bootcamp.database" "service.crud" "bootcamp.commonws" "bootcamp.rest.service.client")
+projets_jars=("bootcamp.common" "bootcamp.database" "service.crud" "bootcamp.commonws")
 
 #Un tableau contenant le nom de tous les projets qui generent un war
-projets_wars=("categorie.service.fonctionnel" "projet.service.fonctionnel" "commentaire.service.fonctionnel" "media.service.fonctionnel" "liketable.service.fonctionnel" "note.service.fonctionnel"  "debat.service.fonctionnel"  "censure.service.fonctionnel" "sondage.service.fonctionnel" "notification.generator.services" "preference.service.fonctionnel" "programme.service.fonctionnel" "projet.service.use.case" "categorie.service.use.case")
+projets_wars=("categorie.service.fonctionnel" "projet.service.fonctionnel" "commentaire.service.fonctionnel" "media.service.fonctionnel" "liketable.service.fonctionnel" "note.service.fonctionnel"  "debat.service.fonctionnel"  "censure.service.fonctionnel" "sondage.service.fonctionnel" "notification.generator.services" "preference.service.fonctionnel" "programme.service.fonctionnel" )
 
 #On verifie si le repertoire dans lequel ont a cloner tous les projets existe
 #Et On sort du script si le repertoire n'existe
@@ -21,15 +21,15 @@ fi
 
 for projet_jar in ${projets_jars[@]}
    do
-      cd "$repository_dir/$projet_jar" ]
+      cd "$repository_dir/$projet_jar"
       mvn clean install deploy -P $profile -DskipTests
 done
 
 for projet_war in ${projets_wars[@]}
    do
-      cd "$repository_dir/$projet_war" ]
-	  rm -rf target/ log*
-      mvn clean package -P $profile
-	  nohup mvn spring-boot:run -P $profile > log.out &
-	  #tail -f log.out
+      cd "$repository_dir/$projet_war"
+      rm -rf target/ log*
+      mvn clean -P $profile
+      nohup mvn spring-boot:run -DskipTests -P $profile > log.out &
+      #tail -f log.out
 done
