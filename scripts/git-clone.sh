a8227007c53e18a3897da897a73218e970caf994
rintiobootcamp <34056371+rintiobootcamp@users.noreply.github.com>
2018-02-05 10:10:56 +0100

    Update git-clone.sh

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -7,7 +7,7 @@ branch=$2
 
 #Un tableau contenant le nom de tous les projets 
 
-projets=("bootcamp.common" "bootcamp.database" "service.crud" "bootcamp.commonws" "bootcamp.rest.service.client" "categorie.service.fonctionnel" "projet.service.fonctionnel" "commentaire.service.fonctionnel" "media.service.fonctionnel" "liketable.service.fonctionnel" "note.service.fonctionnel" "debat.service.fonctionnel"  "censure.service.fonctionnel" "sondage.service.fonctionnel" "notification.generator.services" "notification.diffusion.services" "preference.service.fonctionnel" "programme.service.fonctionnel" "projet.service.use.case" "categorie.service.use.case" "debat.service.use.case" "bootcamp.user" "statistique.service.fonctionnel")
+projets=("bootcamp.common" "bootcamp.database" "service.crud" "bootcamp.commonws" "bootcamp.rest.service.client" "categorie.service.fonctionnel" "projet.service.fonctionnel" "commentaire.service.fonctionnel" "media.service.fonctionnel" "liketable.service.fonctionnel" "note.service.fonctionnel" "debat.service.fonctionnel"  "censure.service.fonctionnel" "sondage.service.fonctionnel" "notification.generator.services" "notification.diffusion.services" "preference.service.fonctionnel" "programme.service.fonctionnel" "projet.service.use.case" "categorie.service.use.case" "debat.service.use.case" "bootcamp.user" "statistique.service.fonctionnel" "bootcamp.security")
 
 #On verifie si le repertoire dans lequel sera cloner tous les projets existe
 #Et On supprime le repertoire si elle existe
